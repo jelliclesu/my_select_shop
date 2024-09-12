cde5d3a1d836306dfa28c2ae10e5ba7cba446e26
jelliclesu <jelliclesu@naver.com>
2024-09-12 22:47:18 +0900

    feat: NaverShopSearch.java 생성 - 네이버 쇼핑 API 이용

---
+++ src/main/java/com/spring/my_select_shop/utils/NaverShopSearch.java
@@ -0,0 +1,30 @@
+package com.spring.my_select_shop.utils;
+
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+public class NaverShopSearch {
+
+    public String search() {
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Naver-Client-Id", "MPEaRGTkuuRkP80VAtpK");
+        headers.add("X-Naver-Client-Secret", "xGUSutAxOz");
+        String body = "";
+
+        HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=iphone", HttpMethod.GET, requestEntity, String.class);
+        HttpStatus httpStatus = (HttpStatus) responseEntity.getStatusCode();
+        int status = httpStatus.value();
+        String response = responseEntity.getBody();
+        System.out.println("Response status: " + status);
+        System.out.println(response);
+
+        return response;
+    }
+
+    public static void main(String[] args) {
+        NaverShopSearch naverShopSearch = new NaverShopSearch();
+        naverShopSearch.search();
+    }
+}
